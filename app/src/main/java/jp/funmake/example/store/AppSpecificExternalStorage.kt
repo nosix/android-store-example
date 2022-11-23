cb3ee1bbce4232bc1e4ca52f195b59a4dab75dd3
nosix <nosix@users.noreply.github.com>
2022-11-23 16:43:57 +0900

    add AppSpecificExternalStorage

---
+++ app/src/main/java/jp/funmake/example/store/AppSpecificExternalStorage.kt
@@ -0,0 +1,82 @@
+package jp.funmake.example.store
+
+import android.content.Context
+import android.os.Environment
+import android.util.Log
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+class AppSpecificExternalStorage {
+
+    private fun runIfMounted(action: AppSpecificExternalStorage.() -> Unit) {
+        val isMounted = Environment.getExternalStorageState() in setOf(
+            Environment.MEDIA_MOUNTED,
+            Environment.MEDIA_MOUNTED_READ_ONLY
+        )
+        if (isMounted) this.action() else {
+            Log.d(TAG, "External storages are not mounted.")
+        }
+    }
+
+    fun create(context: Context) {
+        runIfMounted {
+            // files/Picturesディレクトリを作成する
+            val pictureDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            Log.d(TAG, "create $pictureDir")
+            // files/fileファイルを作成する
+            val filesDir = context.getExternalFilesDir(null)
+            FileOutputStream(File(filesDir, "file")).use {}
+            // files/Pictures/fileファイルを作成する
+            FileOutputStream(File(pictureDir, "file")).use {}
+
+            // cache/Picturesディレクトリを作成する
+            val pictureCacheDir = File(context.externalCacheDir, Environment.DIRECTORY_PICTURES)
+            pictureCacheDir.mkdirs()
+            Log.d(TAG, "create $pictureCacheDir")
+            // cache/Pictures/file*.tmpファイルを作成する
+            File.createTempFile("file", ".tmp", pictureCacheDir)
+        }
+    }
+
+    fun read(context: Context) {
+        runIfMounted {
+            // 全てのfilesDirを取得する
+            ContextCompat.getExternalFilesDirs(context, null).forEach {
+                Log.d(TAG, "externalFilesDirs $it")
+            }
+            // 全てのcacheDirを取得する
+            ContextCompat.getExternalCacheDirs(context).forEach {
+                Log.d(TAG, "externalCacheDirs $it")
+            }
+
+            // filesの一覧をFileで取得する
+            val pictureDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            pictureDir?.listFiles()?.forEach {
+                Log.d(TAG, "fileList $it")
+            }
+            // files/Pictures/fileを読み込む
+            FileInputStream(File(pictureDir, "file")).use {}
+
+            val pictureCacheDir = File(context.externalCacheDir, Environment.DIRECTORY_PICTURES)
+            pictureCacheDir.listFiles()?.forEach {
+                Log.d(TAG, "cacheFileList $it")
+            }
+        }
+    }
+
+    fun delete(context: Context) {
+        runIfMounted {
+            // files/Picturesディレクトリを削除する
+            val pictureDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            pictureDir?.deleteRecursively()
+            // files/fileファイルを削除する
+            val filesDir = context.getExternalFilesDir(null)
+            File(filesDir, "file").delete()
+            // cache/Picturesディレクトリを削除する
+            val pictureCacheDir = File(context.externalCacheDir, Environment.DIRECTORY_PICTURES)
+            pictureCacheDir.deleteRecursively()
+        }
+    }
+}
--- app/src/main/java/jp/funmake/example/store/MainActivity.kt
+++ app/src/main/java/jp/funmake/example/store/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.funmake.example.store
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -12,23 +13,25 @@ class MainActivity : AppCompatActivity() {
     private val scope = CoroutineScope(Dispatchers.IO)
 
     private val appSpecificInternalStorage = AppSpecificInternalStorage()
+    private val appSpecificExternalStorage = AppSpecificExternalStorage()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         scope.launch {
+            Log.d(TAG, "AppSpecificInternalStorage")
             appSpecificInternalStorage.create(this@MainActivity)
             appSpecificInternalStorage.read(this@MainActivity)
             appSpecificInternalStorage.delete(this@MainActivity)
+            Log.d(TAG, "AppSpecificExternalStorage")
+            appSpecificExternalStorage.create(this@MainActivity)
+            appSpecificExternalStorage.read(this@MainActivity)
+            appSpecificExternalStorage.delete(this@MainActivity)
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         scope.cancel()
-    }
-
-    private fun readAppSpecificExternalStorage() {
-
     }
 }
